94c8f3deadef89ac16ac611b206efe8a3123fa4e
matt venn <matt@mattvenn.net>
2015-02-03 17:15:01 +0000

    argparse

--- process.py
+++ process.py
@@ -1,37 +1,68 @@
 #!/usr/bin/python
 import os
 import re
+import argparse
 
-name = 'soundbox'
 
-#process with pandoc
-os.system('pandoc -o {0}.html {0}.md -fmarkdown-implicit_figures'.format(name))
+def make_html():
+    #process with pandoc
+    print("making html")
+    os.system('pandoc -o {0}.html {0}.md -fmarkdown-implicit_figures'.format(args.name))
 
-#upload assets
-os.system("rsync -avz -e 'ssh' --progress assets/ cpdforteachers@cpdforteachers.com:/var/www/vhosts/cpdforteachers.com/website/public/assets/")
+#could do just the ones related to the file
+def upload_assets():
+    #upload assets
+    print("uploading...")
+    os.system("rsync -avz -e 'ssh' --progress assets/ cpdforteachers@cpdforteachers.com:/var/www/vhosts/cpdforteachers.com/website/public/assets/")
 
-# do some search and replace stuff to get the html as we need
-with open("%s.html" % name) as fh:
-    html = fh.readlines()
+def post_process():
 
-out = ''
-for line in html:
-    m = re.search('^<p>button(<a )(href="[^"]+">.*</a>)</p>',line)
-    if m:
-        button_class = 'btn btn-primary'
-        out += '<p>' + m.group(1) + "class='%s' " % button_class + m.group(2) + '</p>'
-        continue
+    print("post process")
+    # do some search and replace stuff to get the html as we need
+    with open("%s.html" % args.name) as fh:
+        html = fh.readlines()
 
-    m = re.search('^<p>scratch<a href="([^"]+)">.*</a></p>',line)
-    if m:
-        out += '<iframe allowtransparency="true" width="485" height="402" src="http://scratch.mit.edu/projects/embed/%s/?autostart=false" frameborder="0" allowfullscreen></iframe>' % m.group(1)
-        continue
+    out = ''
+    for line in html:
+        m = re.search('^<p>button(<a )(href="[^"]+">.*</a>)</p>',line)
+        if m:
+            button_class = 'btn btn-primary'
+            out += '<p>' + m.group(1) + "class='%s' " % button_class + m.group(2) + '</p>'
+            continue
 
-    #otherwise
-    out += line
+        m = re.search('^<p>scratch<a href="([^"]+)">.*</a></p>',line)
+        if m:
+            out += '<iframe allowtransparency="true" width="485" height="402" src="http://scratch.mit.edu/projects/embed/%s/?autostart=false" frameborder="0" allowfullscreen></iframe>' % m.group(1)
+            continue
 
-with open("%s.html" % name, 'w') as fh:
-    fh.write(out)
+        #otherwise
+        out += line
 
-#put into clipboard
-os.system('cat %s.html | xclip -selection c' % name)
+    with open("%s.html" % args.name, 'w') as fh:
+        fh.write(out)
+
+def copy_paste():
+    #put into clipboard
+    os.system('cat %s.html | xclip -selection c' % args.name)
+    print("copied html into clipboard")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="process and upload cpd resources")
+    parser.add_argument('--name', action='store', 
+        help="name of markdown file", required=True)
+    parser.add_argument('--upload', action='store_const', const=True,
+        help="upload content", default=False)
+
+    args = parser.parse_args()
+    
+    #process markdown
+    make_html()
+    #post process html
+    post_process()
+    #upload assets
+    if args.upload:
+        upload_assets()
+    else:
+        print("use --upload to update assets directory on server")
+    #copy html into paste buffer
+    copy_paste()
